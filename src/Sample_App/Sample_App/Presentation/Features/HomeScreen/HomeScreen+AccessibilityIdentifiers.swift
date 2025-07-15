d2cb72a6d724c20964d8dfe307712cbd32a749b9
pradeepmiller <47136634+pradeepmiller@users.noreply.github.com>
2025-07-15 20:19:05 +0530

    Create HomeScreen+AccessibilityIdentifiers.swift

---
+++ src/Sample_App/Sample_App/Presentation/Features/HomeScreen/HomeScreen+AccessibilityIdentifiers.swift
@@ -0,0 +1,16 @@
+
+/*
+ * Copyright (c) 2025 by Sample App. All Rights Reserved.
+ */
+
+import Foundation
+
+extension Setup {
+    enum Accessibility {
+        enum HomeScreen: String, RawRepresentable {
+            case HOME_SCREEN_TEXT_HEADER = "home_screen_text_header"
+            case HOME_SCREEN_TEXT_DESCRIPTION = "home_screen_text_description"
+            case HOME_SCREEN_BUTTON_MENU = "home_screen_button_menu"
+        }
+    }
+}
