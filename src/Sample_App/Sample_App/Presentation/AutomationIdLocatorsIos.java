3c2511e6f97b4d1596edc4afa438ce023e8fe96f
pradeepmiller <47136634+pradeepmiller@users.noreply.github.com>
2025-07-15 15:08:30 +0000

    Convert swift files having text 'AccessibilityIdentifiers' in file name to AutomationIdLocatorsIos.java using Python script

---
+++ src/Sample_App/Sample_App/Presentation/AutomationIdLocatorsIos.java
@@ -0,0 +1,25 @@
+/*
+ * Auto-generated Java file from Swift Accessibility Identifiers for iOS platform
+ */
+public class AutomationIdLocatorsForIos {
+
+    public static class HomeScreen {
+        public static final String HOME_SCREEN_BUTTON_MENU = "home_screen_button_menu";
+        public static final String HOME_SCREEN_TEXT_DESCRIPTION = "home_screen_text_description";
+        public static final String HOME_SCREEN_TEXT_HEADER = "home_screen_text_header";
+    }
+
+    public static class WelcomeScreen {
+        public static final String WELCOME_SCREEN_TEXT_HEADER = "welcome_screen_text_header";
+        public static final String WELCOME_SCREEN_TEXT_DESCRIPTION = "welcome_screen_text_description";
+        public static final String WELCOME_SCREEN_BUTTON_CONTINUE = "welcome_screen_button_continue";
+    }
+
+    public static class LoginScreen {
+        public static final String LOGIN_SCREEN_BUTTON_SUBMIT = "login_screen_button_submit";
+        public static final String LOGIN_SCREEN_TEXT_HEADER = "login_screen_text_header";
+        public static final String LOGIN_SCREEN_INPUT_EMAIL = "login_screen_input_email";
+        public static final String LOGIN_SCREEN_INPUT_PASSWORD = "login_screen_input_password";
+    }
+
+}
