0958e9b4a6f0eec692450558e59d841d14c3cd7a
pradeepmiller <47136634+pradeepmiller@users.noreply.github.com>
2025-07-15 16:37:17 +0530

    Update Setup+AccessibilityIdentifiers.swift

--- src/Sample_App/Sample_App/Presentation/Features/Setup/Setup+AccessibilityIdentifiers.swift
+++ src/Sample_App/Sample_App/Presentation/Features/Setup/Setup+AccessibilityIdentifiers.swift
@@ -1,1 +1,23 @@
 
+/*
+ * Copyright (c) 2025 by Sample App. All Rights Reserved.
+ */
+
+import Foundation
+
+extension Setup {
+    enum Accessibility {
+        enum WelcomeScreen: String, RawRepresentable {
+            case WELCOME_SCREEN_TEXT_HEADER = "welcome_screen_text_header"
+            case WELCOME_SCREEN_TEXT_DESCRIPTION = "welcome_screen_text_description"
+            case WELCOME_SCREEN_BUTTON_CONTINUE = "welcome_screen_button_continue"
+        }
+
+        enum LoginScreen: String, RawRepresentable {
+            case LOGIN_SCREEN_TEXT_HEADER = "login_screen_text_header"
+            case LOGIN_SCREEN_INPUT_EMAIL = "login_screen_input_email"
+            case LOGIN_SCREEN_INPUT_PASSWORD = "login_screen_input_password"
+            case LOGIN_SCREEN_BUTTON_SUBMIT = "login_screen_button_submit"
+        }
+    }
+}
